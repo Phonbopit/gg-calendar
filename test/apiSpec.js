6801d175709e4b2a3f020b961d40c88e400ff495
Chai Phonbopit <phonbopit@gmail.com>
2016-05-14 17:13:11 +0700

    add lib

--- test/apiSpec.js
+++ test/apiSpec.js
@@ -1,11 +1,20 @@
 import { expect } from 'chai';
 import fs from 'fs';
+import { authorize } from '../lib';
 
 describe('Calendar API', () => {
   it('should provide client_secret.json file', (done) => {
     fs.readFile('client_secret.json', (err, content) => {
       expect(err).to.be.null;
       expect(content).to.be.an.object;
+      done();
+    });
+  });
+
+  it('should autozied passed with credentials file', (done) => {
+    authorize((err, auth) => {
+      expect(err).to.be.null;
+      expect(auth.credentials.access_token).to.be.a.string;
       done();
     });
   });
